2a98b09ae20e46674f0837a9fa523a96bf158548
maruel <>
2005-04-11 15:41:20 +0000

    updated resources

--- activex/resource.h
+++ activex/resource.h
@@ -6,7 +6,15 @@
 #define IDR_A5250                       101
 #define IDB_TERM5250                    105
 #define IDR_TERM5250                    106
+#define IDC_EDIT1                       201
+#define IDC_EDIT_IP                     201
 #define IDD_DIALOG1                     202
+#define IDD_DIALOG_CONNECT              202
+#define IDC_EDIT_DEVICE                 202
+#define IDC_EDIT_CODE_PAGE              203
+#define IDC_RADIO_24                    204
+#define IDC_RADIO2                      205
+#define IDC_RADIO_27                    205
 #define ID_FILE_EXIT                    32768
 
 // Next default values for new objects
@@ -15,7 +23,7 @@
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        203
 #define _APS_NEXT_COMMAND_VALUE         32769
-#define _APS_NEXT_CONTROL_VALUE         201
+#define _APS_NEXT_CONTROL_VALUE         206
 #define _APS_NEXT_SYMED_VALUE           107
 #endif
 #endif
