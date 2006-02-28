ddf97f18c7db194004a85e8b0c07c13e16555c91
maruel <>
2006-02-28 03:13:09 +0000

    Now builds with the new hierarchy

---
+++ activex/config.h
@@ -0,0 +1,5 @@
+#pragma once
+#pragma warning(disable:4255) // '' : no function prototype given: converting '' to ''
+#pragma warning(disable:4668) // '' is not defined as a preprocessor macro, replacing with '0' for '#if/#elif'
+#pragma warning(disable:4820) // '' : 'N' bytes padding added after member ''
+#include "tn5250-config.h"
